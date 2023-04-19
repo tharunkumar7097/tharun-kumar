8ea05a536560321da85aba6b573828fabb1134a1
Tharunkumar <tharunkumar7097@gmail.com>
2023-04-19 10:33:17 +0530

    first commit

---
+++ program.py
@@ -0,0 +1,13 @@
+num_1=int(input("Enter first number"))
+num_2=int(input("Enter second number"))
+operator=input("Enter the Arithmetic operator:")
+if operator== "+":
+    print("The sum of",num_1,"and",num_2,"is:",num_1+num_2)
+if operator== "-":
+    print("The difference of",num_1,"and",num_2,"is:",num_1-num_2)
+if operator== "*":
+    print("The product of",num_1,"and",num_2,"is:",num_1*num_2)
+if operator== "/":
+    print("The quotient of",num_1,"and",num_2,"is:",num_1//num_2)
+if operator== "%":
+    print("The remainder of",num_1,"and",num_2,"is:",num_1%num_2)
